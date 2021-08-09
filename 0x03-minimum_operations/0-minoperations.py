1fe624f8613e4a27a38d081e79158adbc6ba60c8
faspen <2335@holbertonschool.com>
2021-08-09 19:03:07 +0000

    minops

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Minimum ops"""
+
+
+def minOperations(n):
+    """Return minimum number of operations needed"""
+    if n < 2:
+        return 0
+
+    ops = 0
+    h = 1
+    printed = 0
+
+    while h < n:
+        if n % h == 0:
+            printed = h
+            ops += 1
+        h += printed
+        ops += 1
+
+    if h == n:
+        return ops
+
+    return 0
