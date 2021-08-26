95c2bafb0ecf4a3c2dc39d9c6ec880fcc19f1737
faspen <2335@holbertonschool.com>
2021-08-26 15:13:21 +0000

    added func

--- 0x05-linked_list_palindrome/lists.h
+++ 0x05-linked_list_palindrome/lists.h
@@ -18,7 +18,7 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
+int pal_help(listint_t **left, listint_t *right);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
