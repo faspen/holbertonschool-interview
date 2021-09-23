e66c404237d890c717fcbf2419ae5886bef5a002
faspen <2335@holbertonschool.com>
2021-09-23 15:32:54 +0000

    0-validate_utf8.py

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,20 +4,22 @@
 
 def validUTF8(data):
     """Utf8 validator"""
-    try:
-        valid = True
-        for i in data:
-            i = i & 0xff
-            if i >= 192 and i < 244:
-                valid = False
-            if i >= 224 and i < 240:
-                valid = False
-            if i >= 240 and i < 248:
-                valid = False
-            if i >= 128 and i < 192:
-                valid = False
-            if i > 255:
-                valid = False
-        return valid
-    except Exception as j:
-        return False
+    val = False
+    trav = 0
+
+    for i in data:
+        byte = format(i, '#010b')[-8:]
+        if trav == 0:
+            if byte[0] == '1':
+                trav = len(byte.split('0')[0])
+            if trav > 4 or trav == 1:
+                return False
+            if trav == 0:
+                continue
+        else:
+            if not byte.startswith('10'):
+                return False
+        trav = trav - 1
+    if trav == 0:
+        return True
+    return val
