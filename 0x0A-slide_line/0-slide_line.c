0976a54d24366aee8a30b6644ec65e90a369a4dc
faspen <2335@holbertonschool.com>
2021-09-27 18:43:44 +0000

    0-slide_line.c

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,23 +1,79 @@
 #include "slide_line.h"
 
 /**
- * slide_left - slide numbers to the left
- * @line: line to slide
- * @size: number of elements
- */
-void slide_left(int *line, size_t size)
-{
-
-}
-
-/**
  * slide_right - slide numbers to the right
  * @line: line to slide
  * @size: number of elements
  */
 void slide_right(int *line, size_t size)
 {
+	size_t i, j;
 
+	for (i = size; i > 0; i--)
+	{
+		for (j = i - 1; j > 0; j--)
+		{
+			if (line[i - 1] != 0)
+			{
+				if (line[i - 1] == line[j - 1])
+				{
+					line[i - 1] = line[i - 1] + line[j - 1];
+					line[j - 1] = 0;
+					break;
+				}
+				if (line[i - 1] != line[j - 1] && line[j - 1] != 0)
+					break;
+			}
+			if (line[i - 1] == 0)
+			{
+				if (line[j - 1] != 0)
+				{
+					line[i - 1] = line[j - 1];
+					line[j - 1] = 0;
+					i++;
+					break;
+				}
+			}
+		}
+	}
+}
+
+/**
+ * slide_left - slide numbers to the left
+ * @line: line to slide
+ * @size: number of elements
+ */
+void slide_left(int *line, size_t size)
+{
+	size_t i, j, z;
+
+	for (i = 0, j = 0; i < size;)
+	{
+		if (line[i] == 0)
+			for (; line[i] == 0;)
+				i++;
+		if (i == size - 1)
+			break;
+
+		j = i + 1;
+		if (line[j] == 0)
+			for (; line[j] == 0;)
+				j++;
+		if (line[i] == line[j])
+		{
+			line[i] = line[i] + line[j];
+			line[j] = 0;
+			i = j + 1;
+		}
+		else
+			i++;
+		j = 0;
+	}
+	for (i = 0, z = 0; i < size; i++)
+		if (line[i] != 0)
+			line[z++] = line[i];
+	while (z < size)
+		line[z++] = 0;
 }
 
 /**
@@ -25,6 +81,7 @@ void slide_right(int *line, size_t size)
  * @line: points to array of ints
  * @size: number of elements
  * @direction: left or right
+ * Return: 1 on success, 0 on fail
  */
 int slide_line(int *line, size_t size, int direction)
 {
