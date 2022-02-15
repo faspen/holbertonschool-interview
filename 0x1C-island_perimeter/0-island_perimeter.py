9bea72d4a2e88d92dda259a770add2b952443b5d
faspen <2335@holbertonschool.com>
2022-02-15 16:45:52 +0000

    island perimeter

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Island Perimeter"""
+
+
+def island_perimeter(grid):
+    """Find perimeter of the island"""
+    m, n = len(grid), len(grid[0])
+    land, adj = 0, 0
+
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == 1:
+                land += 1
+
+                if (i < m - 1) and grid[i + 1][j] == 1:
+                    adj += 1
+                if (j < n - 1) and grid[i][j + 1] == 1:
+                    adj += 1
+
+    return ((land * 4) - (2 * adj))
