e9d52a548a34c53105b06805d51f386873f6a82e
faspen <aspensonf@gmail.com>
2022-03-02 11:07:46 -0600

    parenth around returns

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -4,20 +4,20 @@
  * wildcmp - sees if two strings are roughly the same
  * @s1: first string
  * @s2: compare string
- * 
+ *
  * Return: 1 if the same, 0 if not
  */
 int wildcmp(char *s1, char *s2)
 {
     if (*s1 == *s2 && *s1 != '\0')
-        return wildcmp(s1 + 1, s2 + 1);
+        return (wildcmp(s1 + 1, s2 + 1));
     else if (*s1 == *s2)
         return (1);
 
     else if (*s2 == '*' && *(s2 + 1) != *s1 && *(s2 + 1) != '*')
-        return wildcmp(s1 + 1, s2);
+        return (wildcmp(s1 + 1, s2));
     else if (*s2 == '*' && *(s2 + 1) != *s1 && *(s2 + 1) == '*')
-        return wildcmp(s1, s2 + 1);
+        return (wildcmp(s1, s2 + 1));
     else if (*s2 == '*' && wildcmp(s1, s2 + 1) == 1)
         return (1);
     else if (*s2 == '*' && wildcmp(s1, s2 + 1) == 0)
